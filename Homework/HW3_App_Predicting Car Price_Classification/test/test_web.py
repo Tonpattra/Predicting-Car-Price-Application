5725562601eead0cd5adc8a7ac97c5a125279551
Ipcount <pattranit210343@gmail.com>
2023-10-01 22:24:24 +0700

    test

---
+++ Homework/HW3_App_Predicting Car Price_Classification/test/test_web.py
@@ -0,0 +1,47 @@
+import unittest
+from flask import Flask
+import sys
+import os
+import requests
+import pytest
+
+current_dir = os.path.dirname(__file__)
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+print(os.listdir())
+
+from app.app import app  
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    client = app.test_client()
+    yield client
+
+def test_homepage_response(client):
+    response = client.get('/')
+    assert response.status_code == 200  # Check if the homepage responds with a 200 status code
+    print("Homepage Test Passed")  # Print a message indicating the test passed
+
+def test_website1_response(client):
+    response = client.get('/website1')
+    assert response.status_code == 200  # Check if website1 responds with a 200 status code
+    print("Website1 Test Passed")  # Print a message indicating the test passed
+
+def test_website2_response(client):
+    response = client.get('/website2')
+    assert response.status_code == 200  # Check if website2 responds with a 200 status code
+    print("Website2 Test Passed")  # Print a message indicating the test passed
+
+def test_receive_data_post_response(client):
+    data = {
+        'engine': '1248',
+        'maxpower': '83.14',
+        'year': '2016'
+    }
+    response = client.post('/receive_data', data=data)
+    assert response.status_code == 200  # Check if the post request to receive_data responds with a 200 status code
+    print("Receive Data Test Passed")  # Print a message indicating the test passed
+
+if __name__ == '__main__':
+    pytest.main()
